c7cadd83b09837228e9b2c4e92b88415b3a38862
Christopher Molin <gusmolich@student.gu.se>
2022-03-07 17:50:08 +0100

    NewEntryScreen: Added date to EHR-object

--- server/jsonHandling/ehrEntry.js
+++ server/jsonHandling/ehrEntry.js
@@ -5,26 +5,11 @@
  */
 export default class EhrEntry {
 
-    // NOT IN USE CURRENTLY: currently only constructor with no parameters is being used. 
-    /**
-     * @param {string} patientID - The id of the patient.
-     * @param {string} healthcareInstitution - The healthcare institution writing this EHR entry.
-     * @param {string} medicalPersonnel - The name of the medical personnel writing this EHR entry.
-     * @param {string} details - The freetext detailing the events of the meeting that this entry is for.
-     * @param {string[]} diagnoses - array of diagnoses made in this entry. 
-     * @param {string[]} prescriptions - array of prescriptions issued in this entry. 
-     */
-    /*
-    constructor(patientID, healthcareInstitution, medicalPersonnel, details, diagnoses, prescriptions) {
-        this.patientID = patientID;
-        this.healthcareInstitution = healthcareInstitution;
-        this.medicalPersonnel = medicalPersonnel;
-        this.details = details;
-        this.diagnoses = diagnoses;
-        this.prescriptions = prescriptions;
+    /** @param {string} date - The date this entry was created, in ISO 8601 format. */
+    setDate(date) {
+        this.date = date;
     }
-    */
-
+    
     /** @param {string} patientID - The id of the patient. */
     setPatientID(patientID) {
         this.patientID = patientID;
