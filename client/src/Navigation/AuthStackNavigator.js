b0598e9a2dfe2b506992b1ec96e0f766d862b6fa
Christopher Molin <gusmolich@student.gu.se>
2022-03-20 01:58:19 +0100

    About-page now accessible globally

--- client/src/Navigation/AuthStackNavigator.js
+++ client/src/Navigation/AuthStackNavigator.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { LoginScreen } from "../screens/LoginScreen/LoginScreen";
+import { AboutScreen } from "../screens/MiscScreens/AboutScreen";
 
 const AuthStack = createStackNavigator();
 const LoginStack = createStackNavigator();
@@ -29,6 +30,13 @@ export function AuthStackNavigator() {
               }}
               name="Login"
               component={LoginScreen}
+            />
+            <LoginStack.Screen
+              options={{
+                headerShown: false,
+              }}
+              name="AboutScreen"
+              component={AboutScreen}
             />
           </LoginStack.Navigator>
         )}
