51f22c6b8226c48f6e51daa11c488c3658eb22a7
hjernkrook <hampus.e.jernkrook@gmail.com>
2022-03-25 19:33:31 +0100

    wrote a first test but currently does not run with truffle test due to ESM modules everywhere else...

--- test/testBlock4EHR.cjs
+++ test/testBlock4EHR.cjs
@@ -1,31 +1,43 @@
 const Block4EHR = artifacts.require("Block4EHR");
 
+const inst_gbg = "i_gbg";
+const inst_boras = "i_boras";
+const inst_kungalv = "i_kungalv";
+const doc_gbg = "d_gbg";
+const doc_kungalv = "d_kungalv";
+const doc_boras = "d_boras";
+
+
 contract("Block4EHR", accounts => {
     let instance;
 
     before(async () => {
+        instance = await Block4EHR.deployed();
+
         // ==== ADDING INSTITUTIONS ====
         await instance.addHealthcareInst("i_gbg", "gbg1", "gbg");
         await instance.addHealthcareInst("i_boras", "b1", "boras");
-        await instance.addHealthcareInst("i_kungalv", "k1", "boras");
+        await instance.addHealthcareInst("i_kungalv", "k1", "kungalv");
     
         // ==== ADDING MEDICAL PERSONNEL ====
         await instance.addMedicalPersonnel(accounts[1], "d_gbg", "i_gbg");
         await instance.addMedicalPersonnel(accounts[2], "d_boras", "i_boras");
-        await instance.addMedicalPersonnel(accounts[3], "d_boras", "i_boras");
+        await instance.addMedicalPersonnel(accounts[3], "d_kungalv", "i_kungalv");
 
         // ==== ADDING PATIENTS ====
-        await instance.addPatient(accounts[9], "p_gbg", ["gbg", "kungalv"]);
+        await instance.addPatient(accounts[9], "p_gbg_kungalv", ["gbg", "kungalv"]);
         await instance.addPatient(accounts[8], "p_boras", ["boras"]);
+        await instance.addPatient(accounts[7], "p_kungalv", ["kungalv"]);
     });
 
     describe("hasPermission returns the correct value", function() {
-        it("hasPermission return true for a sender within a permissioned region", async function() {
-
+        it("hasPermission return true for the patient itself", async function() {
+            let res = await instance.hasPermission("p_boras", {from: accounts[8]});
+            assert.ok(res);
         });
     });
 })
-
+/*
 contract("Block4EHR", accounts => {
     
 })
@@ -33,3 +45,4 @@ contract("Block4EHR", accounts => {
 contract("Block4EHR", accounts => {
     
 })
+*/
