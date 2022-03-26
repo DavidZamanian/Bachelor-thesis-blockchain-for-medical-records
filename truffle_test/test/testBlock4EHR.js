4e187a709066c8a0e5b6a408ef46d27a88e06f15
hjernkrook <hampus.e.jernkrook@gmail.com>
2022-03-26 10:17:35 +0100

    wrote tests for the smart contract but needs to be added to

--- truffle_test/test/testBlock4EHR.js
+++ truffle_test/test/testBlock4EHR.js
@@ -1,42 +1,102 @@
 const Block4EHR = artifacts.require("Block4EHR");
-//import {Block4EHR} from "../contracts/Block4EHR.sol";
 
-const inst_gbg = "i_gbg";
-const inst_boras = "i_boras";
-const inst_kungalv = "i_kungalv";
-const doc_gbg = "d_gbg";
-const doc_kungalv = "d_kungalv";
-const doc_boras = "d_boras";
-
-
+/**
+ * Test for the Block4EHR smart contract. 
+ * @author Hampus Jernkrook
+ */
 contract("Block4EHR", accounts => {
     let instance;
+
+    const inst_gbg = "i_gbg";
+    const inst_boras = "i_boras";
+    const inst_kungalv = "i_kungalv";
+    const doc_gbg = "d_gbg";
+    const doc_kungalv = "d_kungalv";
+    const doc_boras = "d_boras";
+    const pat_gbg_kungalv = "p_gbg_kungalv";
+    const pat_kungalv = "p_kungalv";
+    const pat_boras = "p_boras";
+    const gbg = "gbg";
+    const boras = "boras";
+    const kungalv = "kungalv";
+    const pat_gbg_kungalv_accounts = accounts[9];
+    const pat_boras_account = accounts[8];
+    const pat_kungalv_account = accounts[7];
+    const doc_gbg_account = accounts[1];
+    const doc_boras_account = accounts[2];
+    const doc_kungalv_account = accounts[3];
 
     before(async () => {
         instance = await Block4EHR.deployed();
 
         // ==== ADDING INSTITUTIONS ====
-        await instance.addHealthcareInst("i_gbg", "gbg1", "gbg");
-        await instance.addHealthcareInst("i_boras", "b1", "boras");
-        await instance.addHealthcareInst("i_kungalv", "k1", "kungalv");
+        await instance.addHealthcareInst(inst_gbg, "gbg1", "gbg");
+        await instance.addHealthcareInst(inst_boras, "b1", "boras");
+        await instance.addHealthcareInst(inst_kungalv, "k1", "kungalv");
     
         // ==== ADDING MEDICAL PERSONNEL ====
-        await instance.addMedicalPersonnel(accounts[1], "d_gbg", "i_gbg");
-        await instance.addMedicalPersonnel(accounts[2], "d_boras", "i_boras");
-        await instance.addMedicalPersonnel(accounts[3], "d_kungalv", "i_kungalv");
+        await instance.addMedicalPersonnel(doc_gbg_account, doc_gbg, inst_gbg);
+        await instance.addMedicalPersonnel(doc_boras_account, doc_boras, inst_boras);
+        await instance.addMedicalPersonnel(doc_kungalv_account, doc_kungalv, inst_kungalv);
 
         // ==== ADDING PATIENTS ====
-        await instance.addPatient(accounts[9], "p_gbg_kungalv", ["gbg", "kungalv"]);
-        await instance.addPatient(accounts[8], "p_boras", ["boras"]);
-        await instance.addPatient(accounts[7], "p_kungalv", ["kungalv"]);
+        await instance.addPatient(pat_gbg_kungalv_accounts, pat_gbg_kungalv, [gbg, kungalv]);
+        await instance.addPatient(pat_boras_account, pat_boras, [boras]);
+        await instance.addPatient(pat_kungalv_account, pat_kungalv, [kungalv]);
     });
 
-    //describe("hasPermission returns the correct value", function() {
-        it("hasPermission return true for the patient itself", async function() {
-            let res = await instance.hasPermission("p_boras", {from: accounts[8]});
+    describe("hasPermission returns the correct value", function() {
+        it("hasPermission returns true for the patient itself", async function() {
+            let res = await instance.hasPermission(pat_boras, {from: pat_boras_account});
             assert.ok(res);
         });
-    //});
+
+        it("hasPermission returns true for a doctor within a permissioned region", async function() {
+            let res = await instance.hasPermission(pat_boras, {from: doc_boras_account});
+            assert.ok(res);
+        });
+
+        it("hasPermission returns false for a sender who does not belong to a permissioned region", async function() {
+            let res = await instance.hasPermission(pat_boras, {from: doc_gbg_account});
+            assert.strictEqual(res, false);
+        });
+    });
+
+    describe("getPermissionedRegion sets the permission correctly", function() {
+        it("getPermissionedRegions returns the correct array of regions when called by the patient", async function() {
+            let res = await instance.getPermissionedRegions(pat_gbg_kungalv, {from: pat_gbg_kungalv_accounts});
+            assert.ok(res);
+        });
+
+        //TODO: write test for checking that it throws on the wrong sender
+    });
+
+
+    describe("setPermissions sets the permission correctly", function() {
+        it("setPermissions sets the permission array correctly when the patient removes a region", async function() {
+            let arr = [gbg];
+            await instance.setPermissions(pat_gbg_kungalv, arr, {from: pat_gbg_kungalv_accounts});
+            let res = await instance.getPermissionedRegions(pat_gbg_kungalv, {from: pat_gbg_kungalv_accounts});
+            assert.deepEqual(res, arr);
+        });
+
+/* TODO FIX THIS TEST:
+        it("setPermissions sets the permission array correctly when the patient adds a region", async function() {
+            let arr = await instance.getPermissionedRegions(pat_boras, {from: pat_boras_account});
+            arr.push(kungalv); //TODO: arr is an object, not an array so this cannot be done. Throws an error. 
+            let res = await instance.getPermissionedRegions(pat_boras, {from: pat_boras_account});
+            assert.deepEqual(res, arr);
+        });
+*/
+
+        //TODO: write test for checking that it throws on the wrong sender
+    });
+
+    /*
+        TODO: either here or in separate contract section:
+            - test updating EHR
+            - test getting the EHR CID
+    */
 })
 /*
 contract("Block4EHR", accounts => {
