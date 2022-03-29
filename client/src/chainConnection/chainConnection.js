5fc02e3651cb31312f37c3bce6812f6082460c17
hjernkrook <hampus.e.jernkrook@gmail.com>
2022-03-29 11:35:34 +0200

    started implementing chainConnection in the frontend

---
+++ client/src/chainConnection/chainConnection.js
@@ -0,0 +1,36 @@
+import React, {Component} from "react";
+import Block4EHR from "../../../build/contracts/Block4EHR.json";
+import getWeb3 from "./getWeb3.js";
+
+class ChainConnection extends Component {
+    state = { storageValue: 0, web3: null, accounts: null, contract: null };
+
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      const web3 = await getWeb3();
+
+      // Use web3 to get the user's accounts.
+      const accounts = await web3.eth.getAccounts();
+
+      // Get the contract instance.
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Block4EHR.networks[networkId];
+      const instance = new web3.eth.Contract(
+        Block4EHR.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+
+      // Set web3, accounts, and contract to the state, and then proceed with an
+      // example of interacting with the contract's methods.
+      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
+    }
+  };
+
+}
