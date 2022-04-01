a257daf6d512aad77b0a415edcd0b903a2f1edf3
DavidZamanian <54624136+DavidZamanian@users.noreply.github.com>
2022-04-01 17:20:38 +0200

    Just junk

--- migrations/2_Block4EHR_migration.js
+++ migrations/2_Block4EHR_migration.js
@@ -1,34 +1,64 @@
+import React, { useState } from "react";
+import { database } from "../../../firebaseSetup";
 const Block4EHR = artifacts.require("Block4EHR");
 
 /**
- * Script for deploying the smart contract Block4EHR to the blockchain. 
- * @param {*} deployer 
- * @param {*} network 
- * @param {*} accounts 
- * 
+ * Script for deploying the smart contract Block4EHR to the blockchain.
+ * @param {*} deployer
+ * @param {*} network
+ * @param {*} accounts
+ *
  * @author Hampus Jernkrook
  */
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(Block4EHR).then( async () => {
+  deployer.deploy(Block4EHR).then(async () => {
     let instance = await Block4EHR.deployed();
 
     // ==== ADDING INSTITUTIONS ====
     await instance.addHealthcareInst("iy", "g", "gbg");
     await instance.addHealthcareInst("in", "b", "boras");
-  
+
     // ==== ADDING MEDICAL PERSONNEL ====
     await instance.addMedicalPersonnel(accounts[1], "dy", "iy");
     await instance.addMedicalPersonnel(accounts[2], "dn", "in");
-    
 
     // ==== ADDING PATIENTS ====
     await instance.addPatient(accounts[9], "p_gbg", ["gbg", "kungalv"]);
     await instance.addPatient(accounts[8], "p_boras", ["boras"]);
-
-
   });
 };
+//Not done yet
+const [institutions, setInstitutions] = React.useState([]);
+const [patients, setpatients] = React.useState([]);
 
+let dbRef = ref(database);
+const allInstitutions = [];
+const getInstitutions = async () => {
+  const snapshot = await get(child(dbRef, "Institutions/"));
+  snapshot.array.forEach((element) => {});
+};
+
+//Not gonna use this, irrelevent
+/*
+React.useEffect(() => {
+  const subscriber = onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      setUser(user);
+      let dbRef = ref(database);
+      const snapshot = await get(
+        child(dbRef, "mapUser/" + auth.currentUser.uid)
+      );
+      setUserSSN(snapshot.val().SSN);
+      setRole(snapshot.val().role);
+    } else {
+      setUser();
+      setRole("");
+      setUserSSN("");
+    }
+  });
+  return subscriber;
+}, []);
+*/
 
 /* SOME COMMANDS FOR TESTING THIS IN THE TRUFFLE COMMAND LINE:
 
