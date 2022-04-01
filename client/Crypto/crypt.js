13e8596e00122b4a4d9d6f1f57938609a08671f2
Wendy <pau.vendy@gmail.com>
2022-04-01 09:39:54 +0200

    Added tests for crypt w/ Nils

--- client/Crypto/crypt.js
+++ client/Crypto/crypt.js
@@ -18,22 +18,20 @@ const algorithm = "aes-128-ctr";
  */
 function encryptEHR(record_key, EHR, medPers_privKey) {
   const decrypted_record_key = decryptRecordKey(record_key, medPers_privKey);
-
   const iv = crypto.randomBytes(16);
 
   let cipher = crypto.createCipheriv(
     algorithm,
-    Buffer.from(decrypted_record_key),
+    Buffer.from(decrypted_record_key, "base64"),
     iv
   );
 
-  let encryptedEHR = cipher.update(EHR);
-
+  let encryptedEHR = cipher.update(Buffer.from(EHR, "base64"));
   encryptedEHR = Buffer.concat([encryptedEHR, cipher.final()]);
 
   return {
-    iv: iv.toString("hex"),
-    encryptedData: encryptedEHR.toString("hex"),
+    iv: iv.toString("base64"),
+    encryptedData: encryptedEHR.toString("base64"),
   };
 }
 
@@ -42,16 +40,16 @@ function encryptEHR(record_key, EHR, medPers_privKey) {
  * @param {*} record_key Encrypted record_key retrieved from DB.
  * @param {*} EHR Contains the IV and encrypted EHR.
  * @param {*} privateKey Permissioned private key used to decrypt the record key.
- * @returns The decrypted EHR.
+ * @returns The decrypted EHR in string.
  */
 function decryptEHR(record_key, EHR, privateKey) {
   const decrypted_record_key = decryptRecordKey(record_key, privateKey);
-  let iv = Buffer.from(EHR.iv, "hex");
-  let encryptedEHR = Buffer.from(EHR.encryptedData, "hex");
+  let iv = Buffer.from(EHR.iv, "base64");
+  let encryptedEHR = Buffer.from(EHR.encryptedData, "base64");
 
   let decipher = crypto.createDecipheriv(
     algorithm,
-    Buffer.from(decrypted_record_key),
+    Buffer.from(decrypted_record_key, "base64"),
     iv
   );
 
@@ -66,16 +64,18 @@ function decryptEHR(record_key, EHR, privateKey) {
 /**
  * Encrypts the record key using asymmetric encryption. Also used to give permission to the EHR by which users' public key is used.
  * Creating a function to encrypt string
- * @param {*} plaintext A generated symmetric record key.
+ * @param {*} record_key A generated symmetric record key.
  * @param {*} publicKeyFile The public key used to encrypt the record key.
  * @returns Encrypted record key.
  */
-function encryptRecordKey(plaintext, publicKeyFile) {
+function encryptRecordKey(record_key, publicKeyFile) {
   const publicKey = fs.readFileSync(publicKeyFile, "utf8");
 
   // publicEncrypt() method with its parameters
-  const encrypted = crypto.publicEncrypt(publicKey, Buffer.from(plaintext));
-
+  const encrypted = crypto.publicEncrypt(
+    publicKey,
+    Buffer.from(record_key, "base64")
+  );
   return encrypted.toString("base64");
 }
 
@@ -83,39 +83,23 @@ function encryptRecordKey(plaintext, publicKeyFile) {
  * Decrypts the record key using asymmetric encryption.
  * // Creating a function to decrypt string
  * @param {*} record_key Encrypted record key.
- * @param {*} privateKey A users private key.
+ * @param {*} privateKeyFile A users private key.
  * @returns The decrypted record key.
  */
-function decryptRecordKey(record_key, privateKey) {
-  // privateDecrypt() method with its parameters
-  const decrypted = crypto.privateDecrypt(
-    {
-      key: privateKey,
-    },
-    Buffer.from(record_key, "base64")
-  );
-
-  return decrypted.toString("utf8");
-}
-
-function decryptRecordKeyTEST(ciphertext, privateKeyFile) {
+function decryptRecordKey(record_key, privateKeyFile) {
   const privateKey = fs.readFileSync(privateKeyFile, "utf8");
 
   // privateDecrypt() method with its parameters
   const decrypted = crypto.privateDecrypt(
-    {
-      key: privateKey,
-    },
-    Buffer.from(ciphertext, "base64")
+    privateKey,
+    Buffer.from(record_key, "base64")
   );
-
-  return decrypted.toString("utf8");
+  return decrypted.toString("base64");
 }
 
 module.exports = {
   encryptRecordKey,
   decryptRecordKey,
-  decryptRecordKeyTEST,
   encryptEHR,
   decryptEHR,
 };
--- test/testCrypto.js
+++ test/testCrypto.js
@@ -1,49 +1,110 @@
-import crypto from "crypto";
-import crypt from "../client/Crypto/crypt.js";
 import * as assert from "assert";
+import crypto from "crypto";
 import fs from "fs";
+import * as path from "path";
+import crypt from "../client/Crypto/crypt.js";
+import JSONService from "../server/jsonHandling/jsonService.js";
+
+
+/**
+ * Methods for encrypting/decrypting EHR and record keys.
+ * @author David Zamanian, Nils Johnsson, Wendy Pau
+ */
+
+const example_directory = "./test/json_examples";
 
 describe("Test crypto", function () {
-  it("Should return encrypted record_key", function () {
-    var record_key;
-    let plaintext = "Hej Wendy";
+  // Generate keys
+  generateKeyFiles();
 
-    function generateKeyFiles() {
-      const keyPair = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 1024,
-        publicKeyEncoding: {
-          type: "spki",
-          format: "pem",
-        },
-        privateKeyEncoding: {
-          type: "pkcs8",
-          format: "pem",
-        },
-      });
+  let record_key;
 
-      // Creating public and private key file
-      fs.writeFileSync("public_key", keyPair.publicKey);
-      fs.writeFileSync("private_key", keyPair.privateKey);
-    }
-
-    // Generate keys
-    generateKeyFiles();
-
-    var encrypted = crypt.encryptRecordKey(plaintext, "./public_key");
-    var decrypted = crypt.decryptRecordKey(encrypted, "private_key");
-    console.log("Encrypted: " + encrypted + "Decrypted: " + decrypted);
-
-    assert.equal(plaintext, decrypted);
-    assert.doesNotThrow(() => {
-      crypt.encryptRecordKey(plaintext, "./public_key");
-    });
-    assert.doesNotThrow(() => {
-      crypt.decryptRecordKeyTEST(encrypted, "private_key");
-    });
-
+  it("Should be able to encrypt/decrypt record key", function () {
+    // generate symmetric AES key
     crypto.generateKey("aes", { length: 128 }, (err, key) => {
       if (err) throw err;
-      record_key = key.export().toString("hex");
+
+      record_key = key;
+      var encrypted_record_key;
+      var decrypted_record_key;
+
+      // check if record key can be encrypted
+      assert.doesNotThrow(() => {
+        encrypted_record_key = crypt.encryptRecordKey(
+          key.export(),
+          "./public_key"
+        );
+      });
+
+      // check if record key can be decrypted
+      assert.doesNotThrow(() => {
+        decrypted_record_key = crypt.decryptRecordKey(
+          encrypted_record_key,
+          "./private_key"
+        );
+      });
+
+      // check that record key has not changed through cryptation
+      assert.equal(key.export().toString("base64"), decrypted_record_key);
     });
   });
+
+  it("Can encrypt/decrypt EHR", () => {
+    // get an EHR in json
+    const ehrJSON = JSONService.fromJsonFile(
+      path.join(example_directory, "EHR_entry.json")
+    );
+
+    // convert to string
+    const ehrBuf = Buffer.from(JSON.stringify(ehrJSON));
+
+    var encryptedEHR;
+    var decryptedEHR;
+
+    // encrypt the record key
+    var encrypted_record_key = crypt.encryptRecordKey(
+      record_key.export(),
+      "./public_key"
+    );
+
+    // check if EHR can be encrypted
+    assert.doesNotThrow(() => {
+      encryptedEHR = crypt.encryptEHR(
+        encrypted_record_key,
+        ehrBuf,
+        "./private_key"
+      );
+    });
+
+    // check if EHR can be decrypted
+    assert.doesNotThrow(() => {
+      decryptedEHR = crypt.decryptEHR(
+        encrypted_record_key,
+        encryptedEHR,
+        "./private_key"
+      );
+    });
+
+    // check that EHR has not changed through cryptation
+    assert.equal(ehrBuf, decryptedEHR);
+  });
 });
+
+function generateKeyFiles() {
+  // Generate keypair
+  const keyPair = crypto.generateKeyPairSync("rsa", {
+    modulusLength: 1024,
+    publicKeyEncoding: {
+      type: "spki",
+      format: "pem",
+    },
+    privateKeyEncoding: {
+      type: "pkcs8",
+      format: "pem",
+    },
+  });
+
+  // Creating public and private key file
+  fs.writeFileSync("public_key", keyPair.publicKey);
+  fs.writeFileSync("private_key", keyPair.privateKey);
+}
